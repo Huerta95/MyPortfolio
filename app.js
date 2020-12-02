9850b34db00415af8ff656b375ea4bc8d86c4965
Huerta95 <75396267+Huerta95@users.noreply.github.com>
2020-12-02 15:20:25 -0800

    Update app.js

--- app.js
+++ app.js
@@ -11,25 +11,30 @@ gsap.to('h1 b', {duration: 0, delay: 3.1, text: ".", ease: "none"});
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav_links');
-    const navLinks = document.querySelectorAll('.nav_links li');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
 
-        navLinks.forEach((link, index) => {
-            if(link.style.animation){
-                link.style.animation = '';
-            } else{
-                link.style.animation = `nav_linksFade 0.5s ease forwards ${index /7 + .3}s`;
-            }
-        });
-
-        burger.classList.toggle('toggle');
+        navSlideEffect();
     })
 
-    navLinks.addEventListener('click', () => {
+    nav.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        
+        navSlideEffect();
     })
+}
+
+const navSlideEffect = () => {
+    const navLinks = document.querySelectorAll('.nav_links li');
+
+    navLinks.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation = '';
+        } else{
+            link.style.animation = `nav_linksFade 0.5s ease forwards ${index /7 + .3}s`;
+        }
+    });
 }
 
 const navShadow = () => {
